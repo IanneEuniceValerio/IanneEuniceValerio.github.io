7f3721fad650ea679eb3129f5eb33acc80a012d0
IanneEuniceValerio <202410968@fit.edu.ph>
2025-02-07 21:32:51 +0800

    finished for passing ^^

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -33,15 +33,84 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+ contactMessage = document.getElementById('contact-message')
 
+ const sendEmail = (e) =>{
+    e.preventDefault()
+
+    emailjs.sendForm('service_edkg5me','template_s5tc4kf','#contact-form','wF7o7PwK5bbbUyJmN')
+    .then(() =>{
+        contactMessage.textContent = 'Message sent successfully! :)'
+
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset
+    }, () =>{
+        contactMessage.textContent = 'Message not sent :('
+    })
+
+ }
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollElement = document.getElementById('scroll-up');
+    
+    if (window.scrollY >= 350) {
+        scrollElement.classList.add('show-scroll'); // Add a new class to make it visible
+    } else {
+        scrollElement.classList.remove('show-scroll'); // Remove the class when not needed
+    }
+};
 
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight, 
+        sectionTop = current.offsetTop - 58, 
+        sectionId = current.getAttribute('id'), 
+        sectionsClass = current.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        sectionsClass.classList.add('active-link')
+    }else{
+        sectionsClass.classList.remove('active-link')
+    }
+
+    })
+}
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme()) 
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
